b6d5cd4a4212966ec4f8167238bb344fa7ee4356
Jack Bush <bush.jack@gmail.com>
2017-02-13 22:31:27 +0000

    general cleaning up. middleware still needs some love.

--- app.js
+++ app.js
@@ -10,31 +10,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Load models and connect to database
-// require('./config/db.js')();
-
 // Middleware
-require('./config/middleware.js')(app);
+require('./routes/middleware.js')(app);
 
 // Load controllers and attach them to routes
-require('./config/routes.js')(app);
+require('./routes/index.js')(app);
 
 // Error handler
-require('./config/errorHandler.js')(app);
-
-// Socket!
-
-var server = app.listen(8080);
-var io = require('socket.io').listen(server);
-
-io.sockets.on('connection',
-	function (socket) {
-		console.log('connected to: ' + socket.id);
-		socket.on('mouse', function (data) {
-			// socket.broadcast.emit('mouse', data);
-			io.sockets.emit('mouse', data);
-		});
-	}
-);
+require('./routes/errorHandler.js')(app);
 
 module.exports = app;
