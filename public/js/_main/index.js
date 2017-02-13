20657f35de51b337303fdc5a51655b21f2cca129
Jack Bush <bush.jack@gmail.com>
2017-02-13 21:56:11 +0000

    added full-screen link to constellations

--- public/js/_main/index.js
+++ public/js/_main/index.js
@@ -1,1 +1,14 @@
 require('./_nav.js');
+
+setTimeout(function () {
+	var elFullscreen = document.querySelectorAll('.js-fullscreen-canvas');
+	var canvas = document.querySelector('canvas');
+	console.log(canvas);
+
+	elFullscreen.forEach(function (el) {
+		el.addEventListener('click', function () {
+			canvas.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+			// canvas.mozRequestFullScreen();
+		});
+	});
+}, 1000);
